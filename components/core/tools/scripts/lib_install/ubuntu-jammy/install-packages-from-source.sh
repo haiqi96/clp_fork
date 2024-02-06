0db27858ee501827e22e4136cdfb6bf18c2e72f3
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-02-06 17:19:32 +0000

    quick fix for libinstall

--- components/core/tools/scripts/lib_install/ubuntu-jammy/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/ubuntu-jammy/install-packages-from-source.sh
@@ -6,11 +6,13 @@ set -e
 # Error on undefined variable
 set -u
 
-./tools/scripts/lib_install/fmtlib.sh 8.0.1
-./tools/scripts/lib_install/libarchive.sh 3.5.1
-./tools/scripts/lib_install/lz4.sh 1.8.2
-./tools/scripts/lib_install/mongoc.sh 1.24.4
-./tools/scripts/lib_install/mongocxx.sh 3.8.0
-./tools/scripts/lib_install/msgpack.sh 6.0.0
-./tools/scripts/lib_install/spdlog.sh 1.9.2
-./tools/scripts/lib_install/zstandard.sh 1.4.9
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+"$script_dir"/../fmtlib.sh 8.0.1
+"$script_dir"/../libarchive.sh 3.5.1
+"$script_dir"/../lz4.sh 1.8.2
+"$script_dir"/../mongoc.sh 1.24.4
+"$script_dir"/../mongocxx.sh 3.8.0
+"$script_dir"/../msgpack.sh 6.0.0
+"$script_dir"/../spdlog.sh 1.9.2
+"$script_dir"/../zstandard.sh 1.4.9
