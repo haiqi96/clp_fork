2c5f8cf85dfd6f043b0e5ad61ee2f066fcbac0ba
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-02-06 18:24:48 +0000

    update other scripts

--- components/core/tools/scripts/lib_install/ubuntu-jammy/install-packages-from-source.sh
+++ components/core/tools/scripts/lib_install/ubuntu-jammy/install-packages-from-source.sh
@@ -7,12 +7,13 @@ set -e
 set -u
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+scripts_prefix=$script_dir/..
 
-"$script_dir"/../fmtlib.sh 8.0.1
-"$script_dir"/../libarchive.sh 3.5.1
-"$script_dir"/../lz4.sh 1.8.2
-"$script_dir"/../mongoc.sh 1.24.4
-"$script_dir"/../mongocxx.sh 3.8.0
-"$script_dir"/../msgpack.sh 6.0.0
-"$script_dir"/../spdlog.sh 1.9.2
-"$script_dir"/../zstandard.sh 1.4.9
+$scripts_prefix/fmtlib.sh 8.0.1
+$scripts_prefix/libarchive.sh 3.5.1
+$scripts_prefix/lz4.sh 1.8.2
+$scripts_prefix/mongoc.sh 1.24.4
+$scripts_prefix/mongocxx.sh 3.8.0
+$scripts_prefix/msgpack.sh 6.0.0
+$scripts_prefix/spdlog.sh 1.9.2
+$scripts_prefix/zstandard.sh 1.4.9
