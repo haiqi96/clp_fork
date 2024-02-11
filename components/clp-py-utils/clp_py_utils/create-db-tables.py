42d1bdbcfe0c9b3f941a0d31b8ef26ad68b1717b
haiqi96 <14502009+haiqi96@users.noreply.github.com>
2024-02-11 23:56:34 +0000

    Database hack

--- components/clp-py-utils/clp_py_utils/create-db-tables.py
+++ components/clp-py-utils/clp_py_utils/create-db-tables.py
@@ -14,6 +14,38 @@ logging_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 logging_console_handler.setFormatter(logging_formatter)
 logger.addHandler(logging_console_handler)
 
+from contextlib import closing
+from sql_adapter import SQL_Adapter
+from clp_py_utils.clp_config import (
+    COMPRESSION_JOBS_TABLE_NAME,
+    COMPRESSION_TASKS_TABLE_NAME,
+    Database,
+    SEARCH_JOBS_TABLE_NAME,
+)
+from clp_py_utils.core import read_yaml_config_file
+def drop_tables(config_file_path: str):
+    try:
+        database_config = Database.parse_obj(read_yaml_config_file(config_file_path))
+        if database_config is None:
+            raise ValueError(f"Database configuration file '{config_file_path}' is empty.")
+        sql_adapter = SQL_Adapter(database_config)
+        clp_db_connection_params = database_config.get_clp_connection_params_and_type(True)
+        table_prefix = clp_db_connection_params["table_prefix"]
+        with closing(sql_adapter.create_connection(True)) as clp_db, closing(
+            clp_db.cursor(dictionary=True)
+        ) as clp_db_cursor:
+            clp_db_cursor.execute(f"DROP TABLE IF EXISTS `{table_prefix}files`")
+            clp_db_cursor.execute(f"DROP TABLE IF EXISTS `{table_prefix}archives`")
+            clp_db_cursor.execute(f"DROP TABLE IF EXISTS `{COMPRESSION_TASKS_TABLE_NAME}`")
+            clp_db_cursor.execute(f"DROP TABLE IF EXISTS `{COMPRESSION_JOBS_TABLE_NAME}`")
+
+            clp_db.commit()
+    except:
+        logger.exception("Failed to drop clp database tables.")
+        return -1
+
+    return 0
+
 
 def main(argv):
     args_parser = argparse.ArgumentParser(description="Creates database tables for CLP.")
@@ -21,6 +53,8 @@ def main(argv):
     parsed_args = args_parser.parse_args(argv[1:])
 
     config_file_path = pathlib.Path(parsed_args.config)
+
+    drop_tables(config_file_path)
 
     script_dir = pathlib.Path(__file__).parent.resolve()
 
